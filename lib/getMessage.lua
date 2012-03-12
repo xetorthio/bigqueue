8c7300e1184f183ae342411e9268cf571c248496
Gabriel Eisbruch <geisbruch@gmail.com>
2012-03-12 19:13:07 -0300

    more test

--- lib/getMessage.lua
+++ lib/getMessage.lua
@@ -24,41 +24,62 @@ function isEmpty(t)
     return table.getn(t) <=0
 end
 
-function getMessage(topicKey,messageId)
+function getMessage(messageId)
     return redis.call("hgetall",topicKey..":messages:"..messageId)
 end
 
-function addToProcessing(messageId,expire)
-    redis.call("zadd",consumerKey..":processing",tms,messageId)
+function addToProcessing(messageId)
+    local expire = getExpireTime()
+    redis.call("zadd",consumerKey..":processing",expire,messageId)
 end
+
+function addIdToMessage(msgId,message)
+    table.insert(message,"id")
+    table.insert(message,msgId)
+end
+
 -- Main Code
 
-local lastId = redis.call("get",lastPointer)
-if not lastId then
-    return {err=consumerGroup.." for topic "..topic.." doesn't exist"}
+---- Check for expired id's
+local expired = redis.call("zrangebyscore",processingList,"-inf",tms)
+for k,v in pairs(expired) do
+    redis.call("lpush",failsList,v)
+    redis.call("zrem",processingList,v)
 end
 
+
+
+-- Get take failed message without remove
 local failed = redis.call("lrange",failsList,0,0) 
 
 if isEmpty(failed) then
-    -- Standar flow
-    local message = getMessage(topicKey,lastId)
+    -- Standar flow if no failed found
+   
+    ---- Get last id
+    local msgId = redis.call("get",lastPointer)
+    
+    local message = getMessage(msgId)
     if isEmpty(message) then
         local topicHead = redis.call("get",topicKey..":head")
-        if lastId < topicHead then
+        if msgId < topicHead then
             redis.call("incr",lastPointer)
         end
         return {}
     end
-    local expire = getExpireTime()
-    addToProcessing(lastId,expire)
+    addToProcessing(msgId)
     redis.call("incr",lastPointer)
-    return message
+    addIdToMessage(msgId,message)
+    return message 
 else
-    -- There are fails flow
-    local messageId = getMessage(topicKey,failed[0])
-
-    print("hay fallados")
+    -- if a failed message found
+    local msgId = failed[1]
+    local message = getMessage(msgId)
+    if isEmpty(message) then
+        return {err="Message with id ["..msgId.."] was expired"}
+    end
+    addToProcessing(msgId)
+    addIdToMessage(msgId,message)
+    return message
 end
 
 
--- test/bq_lua_test.js
+++ test/bq_lua_test.js
@@ -253,6 +253,7 @@ describe("Redis lua scripts",function(){
 
     describe("#getMessage",function(){
 
+        var tms = Math.floor(new Date().getTime()/1000)
         beforeEach(function(done){
             redisClient.flushall(function(err,data){
                 redisClient.eval(createTopicScript, 0, "testTopic", function(err,data){
@@ -268,16 +269,83 @@ describe("Redis lua scripts",function(){
             })
         })
 
+        var redisListToObj = function(redisList){
+            var o = {}
+            for(var i=0;i<redisList.length;i=i+2){
+                o[redisList[i]] = redisList[i+1]
+            }
+            return o
+        }
 
         it("should get the message with id equals to topics:topic:consumers:consumer:last if no fails found", function(done){
-
+            redisClient.get("topics:testTopic:consumers:testConsumer:last",function(err,data){
+                var last = data;
+                redisClient.eval(getMessageScript,0,tms,"testTopic","testConsumer","20",function(err,data){
+                    should.not.exist(err)
+                    var obj = redisListToObj(data)
+                    obj.id.should.equal(last)
+                    done()
+                })
+            })
         })
-        it("should put the message id into topics:topic:consumers:consumer:processing with the expiration time equals to tms+visibilityWindow")
-        it("should move to topics:topic:consumers:consumer:fails the expired message into topics:topic:consumers:consumer:processing")
-        it("should get a failed message over an stander message if topics:topic:consumers:consumer:fails is not empty")
+        it("should put the message id into topics:topic:consumers:consumer:processing with the expiration time equals to tms+visibilityWindow",function(done){
+            redisClient.eval(getMessageScript,0,tms,"testTopic","testConsumer","20",function(err,data){
+                should.not.exist(err)
+                var obj = redisListToObj(data)
+                redisClient.zrangebyscore("topics:testTopic:consumers:testConsumer:processing","-inf","+inf","WITHSCORES",function(err,data){
+                    should.not.exist(err)
+                    data[0].should.equal(""+1)
+                    data[1].should.equal(""+(tms+20))
+                    done()
+                })
+            })
+        })
+        it("should move to topics:topic:consumers:consumer:fails the expired message into topics:topic:consumers:consumer:processing",function(done){
+            var expiredTime = tms -1
+            var noExpiredTime = tms + 1
+            redisClient.zadd("topics:testTopic:consumers:testConsumer:processing",noExpiredTime,"1",function(err,data){
+                redisClient.zadd("topics:testTopic:consumers:testConsumer:processing",expiredTime,"2",function(err,data){
+                    redisClient.zadd("topics:testTopic:consumers:testConsumer:processing",expiredTime,"3",function(err,data){
+                        should.not.exist(err)
+                        redisClient.eval(getMessageScript,0,tms,"testTopic","testConsumer","20",function(err,data){
+                            redisClient.lrange("topics:testTopic:consumers:testConsumer:fails",0,-1,function(err,data){
+                                data.should.have.length(2)
+                                data.should.include("2")
+                                data.should.include("3")
+                                done()
+                            })
+                        })
+                    })
+                })
+            })
+        })
+        it("should get a failed message over an stander message if topics:topic:consumers:consumer:fails is not empty",function(done){
+            redisClient.eval(getMessageScript,0,tms,"testTopic","testConsumer","20",function(err,data){
+                var expired = tms-21
+                redisClient.eval(postMessageScript, 0, "testTopic", JSON.stringify({msg:"testMessage"}), function(err, newId){
+                    should.not.exist(err)
+                    redisClient.lpush("topics:testTopic:consumers:testConsumer:fails",newId,function(err,data){
+                        redisClient.eval(getMessageScript,0,expired,"testTopic","testConsumer","20",function(err,data1){
+                            should.not.exist(err)
+                            redisClient.eval(getMessageScript,0,tms,"testTopic","testConsumer","20",function(err,data2){
+                                var obj1 = redisListToObj(data1)
+                                var obj2 = redisListToObj(data2)
+                                should.exist(obj1)
+                                should.exist(obj2)
+                                obj1.id.should.equal(""+newId)
+                                done()
+                            })
+                        })
+                    })
+                })
+            })
+        })
+        it("should put into the processing list a failed message got")
+        it("should throw an error if a failed message was expired")
         it("should increment the topics:topic:consumers:consumer:last after a get")
         it("shouldn't increment the topics:topic:consumers:consumer:last if the message is from the fails list")
         it("shouldn't no increment the topics:topic:consumers:consumer:last if there are no more messages")
+        it("should return null if no object was found")
         it("should fail if topic doesn't exist")
         it("should fail if the consumerGrouo dosn't exist") 
     })
