46bff557dfb4524f8fa129a293f01169d15b77b4
Gabriel Eisbruch <geisbruch@gmail.com>
2012-03-26 16:51:20 -0300

    Move executable scripts to bin folder+

---
+++ bin/orchestrator_launcher.js
@@ -0,0 +1,34 @@
+#!/usr/local/bin/node
+
+var ZK = require("zookeeper"),
+    oc = require("../lib/bq_cluster_orchestrator.js"),
+    bq = require("../lib/bq_client.js")
+
+var externalConfig = process.argv[2]
+
+var zkConfig = {
+        connect: "localhost:2181",
+        timeout: 200000,
+        debug_level: ZK.ZOO_LOG_LEVEL_WARN,
+        host_order_deterministic: false
+    }   
+
+var ocConfig = {
+    "zkClustersPath":"/bq/clusters",
+    "zkConfig":zkConfig,
+    "createNodeClientFunction":bq.createClient,
+    "checkInterval":500
+}
+
+var config 
+if(externalConfig){
+    config = require(externalConfig).orchestratorConfig
+}else{
+    config = ocConfig 
+}
+
+var orch = oc.createOrchestrator(config)
+orch.on("ready",function(){
+    console.log("Orchestrator running")
+})
+
