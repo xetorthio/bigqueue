a61df4847393520b9d9aa34eda9efd0b99628e85
Gabriel Eisbruch <geisbruch@gmail.com>
2012-03-26 16:26:31 -0300

    add configs and examples to cluster config and index.js to be used as module

--- http_launcher.js
+++ http_launcher.js
@@ -1,27 +1,38 @@
+#!/usr/local/bin/node
+
+/**
+ * Executable to create the http api client
+ * If no config found a default config will be used, this confing
+ * will use a redis localhost running at default port
+ */
 var bq = require('./lib/bq_client.js'),
     ZK = require('zookeeper'),
     bqc = require('./lib/bq_cluster_client.js'),
     http_api = require("./http_api.js")
 
-var clusterPath = "/bq/clusters/test"
+var externalConfig = process.argv[2]
 
-var zkConfig = {
-        connect: "localhost:2181",
-        timeout: 200000,
-        debug_level: ZK.ZOO_LOG_LEVEL_WARN,
-        host_order_deterministic: false
-    }   
-
-var bqClientConfig = {
-    "zkConfig":zkConfig,
-    "zkClusterPath":clusterPath,
-    "createClientFunction":bq.createClient
+//Default redis conf
+var redisLocalhost = {
+    host:"127.0.0.1",
+    port:6379
 }
 
+//Default api conf
 var httpApiConfig = {
     "port": 8080,
-    "bqConfig": bqClientConfig, 
-    "bqClientCreateFunction": bqc.createClusterClient
+    "bqConfig": redisLocalhost, 
+    "bqClientCreateFunction": bq.createClient
 }
 
-http_api.startup(httpApiConfig)
+//Check for external config
+var config 
+if(externalConfig){
+    config = require(externalConfig).httpApiConfig
+}else{
+    config = httpConfig
+}
+
+//Run config
+console.log("Using config: "+JSON.stringify(config))
+http_api.startup(config)
